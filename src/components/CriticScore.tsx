6d35299160202cfaf37695131c1d5694af3b0488
Charles Huffman <huffmancharles14@gmail.com>
2023-08-13 14:51:26 -0500

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 92 ? "green" : score > 84 ? "yellow" : "orange";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
