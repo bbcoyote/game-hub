a228e588f2adae5fd252c4e216630bf9f03b68ad
Charles Huffman <huffmancharles14@gmail.com>
2023-08-13 08:12:17 -0500

    set api key in .env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "4be1b4cff3f04effb25c715a65316ace",
+    key: apiKey,
   },
 });
