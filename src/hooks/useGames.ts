4aecdc05b5e8261915c51fb1e27d491e74bf519d
Charles Huffman <huffmancharles14@gmail.com>
2023-08-16 19:02:32 -0500

    refactor extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 // this interface is defining our Platform props to render playstation, xbox, etc
 export interface Platform {
@@ -18,19 +18,16 @@ export interface Game {
 }
 
 // this is a custom hook we are using to manage our state, fetch and handle API requests for games data, errors and loading states
-const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
       params: {
-        genres: selectedGenre?.id,
-        platforms: selectedPlatform?.id,
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
       },
     },
-    [selectedGenre?.id, selectedPlatform?.id]
+    [gameQuery]
   );
 
 export default useGames;
